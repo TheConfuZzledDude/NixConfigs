e3b86e8b29f99ffc318776ca54b8ce1237c94972
TheConfuZzledDude <theconfuzzleddude@gmail.com>
2024-05-02 02:01:13 +0100

    Fixed incorrect env name

--- nixos/services/caddy/default.nix
+++ nixos/services/caddy/default.nix
@@ -3,19 +3,21 @@
   config,
   lib,
   ...
-}: {
+}: let
+  caddy_pkg = pkgs.callPackage ./custom-caddy.nix {
+    externalPlugins = [
+      {
+        name = "caddy-dns/porkbun";
+        repo = "github.com/caddy-dns/porkbun";
+        version = "4267f6797bf6543d7b20cdc8578a31764face4cf";
+      }
+    ];
+    vendorHash = "sha256-Uy2vq+mEIUs5MMVvLy6G7A308MCjoT2SqKv/I0yDK5w="; # Add this, as explained in https://github.com/NixOS/nixpkgs/pull/259275#issuecomment-1763478985
+  };
+in {
   services.caddy = {
     enable = true;
-    package = pkgs.callPackage ./custom-caddy.nix {
-      externalPlugins = [
-        {
-          name = "caddy-dns/porkbun";
-          repo = "github.com/caddy-dns/porkbun";
-          version = "4267f6797bf6543d7b20cdc8578a31764face4cf";
-        }
-      ];
-      vendorHash = "sha256-Uy2vq+mEIUs5MMVvLy6G7A308MCjoT2SqKv/I0yDK5w="; # Add this, as explained in https://github.com/NixOS/nixpkgs/pull/259275#issuecomment-1763478985
-    };
+    package = caddy_pkg;
     configFile = ./Caddyfile;
   };
 
@@ -26,6 +28,10 @@
     owner = config.services.caddy.user;
     group = config.services.caddy.group;
   };
+
+  environment.systemPackages = [
+    caddy_pkg
+  ];
 
   systemd.services.caddy.serviceConfig.EnvironmentFile = lib.mkForce config.sops.secrets.caddy-env.path;
 
