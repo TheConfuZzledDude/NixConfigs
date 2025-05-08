80767d42cb7893d82c77403384e1bc12bbfd85f8
Zachary Freed <theconfuzzleddude@gmail.com>
2025-05-08 18:25:14 +0100

    added vpn to rdtclient

--- nixos/fuzzle-server/services/rdt-client/default.nix
+++ nixos/fuzzle-server/services/rdt-client/default.nix
@@ -11,4 +11,26 @@
     #TODO: Generate this automatically and import it with IFD?
     ./compose.nix
   ];
+
+  systemd.services."podman-rdtclient" = {
+    serviceConfig = {
+      NetworkNamespacePath = "/var/run/netns/mullvad";
+      BindReadOnlyPaths = "/etc/netns/nordvpn/resolv.conf:/etc/resolv.conf:norbind";
+    };
+  };
+
+  systemd.services."proxy-to-rdtclient" = {
+    requires = ["podman-rdtclient.service" "proxy-to-rdtclient.socket"];
+    after = ["podman-rdtclient.service" "proxy-to-rdtclient.socket"];
+    unitConfig.JoinsNamespaceOf = "podman-rdtclient.service";
+    serviceConfig = {
+      ExecStart = "/run/current-system/sw/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:6500";
+      PrivateNetwork = "yes";
+    };
+  };
+
+  systemd.sockets."proxy-to-rdtclient" = {
+    listenStreams = ["6500"];
+    wantedBy = ["sockets.target"];
+  };
 }
